2271e49e2c28d8663d24075c9a3db6b8dde977ee
Yugal Sonparate <24sonparatey@rbunagpur.in>
2025-04-19 20:28:57 +0530

    Add files via upload

---
+++ Oops-TA-2-main/object-oriented-programming-main/exp-3_inheritance/src/Mammal.java
@@ -0,0 +1,10 @@
+class Mammal extends Animal {
+    @Override
+    void eat() {
+        System.out.println("Mammal eats everything!!");
+    }
+
+    void walk() {
+        System.out.println("Mammal walks!!");
+    }
+}
---
+++ Oops-TA-2-main/object-oriented-programming-main/exp-5_packageAndImport/src/mypackage/exp5.java
@@ -0,0 +1,7 @@
+package mypackage;
+
+public class exp5 {
+    public void show() {
+        System.out.println("Hii  from a custom package!");
+    }
+}
